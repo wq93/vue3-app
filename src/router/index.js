78e0aecca3c18ca5e7f200061c023857d3d839bd
wangqi <wangqi@chicv.com>
2024-11-22 17:20:14 +0800

    feat: intie

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(), // 使用 HTML5 的 history 模式
+  routes,
+});
+
+export default router;
